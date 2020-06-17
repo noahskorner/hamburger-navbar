abfe2c1104b0f1816194760a1e3509756f449424
Noah Korner <noahskorner@gmail.com>
2020-06-17 15:12:21 -0700

    Began app.js file to transform navbar on mobile.

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('nav-links li');
+    //Toggle Navbar
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    });
+    //Animate Links
+    navLinks.forEach((link,index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forward ${index / 7}s';
+        console.log(index/7);
+    });
+
 }
+
+navSlide();
